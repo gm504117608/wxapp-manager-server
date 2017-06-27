fc851c01eaf4eb672012ad22148d284ea86f148e
gm504117608 <504117608@qq.com>
2017-06-27 15:54:40 +0800

    first commit

---
+++ wxapp-manager-api/src/main/webapp/index.jsp
@@ -0,0 +1,17 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: wangdawei
+  Date: 15/11/12
+  Time: 上午11:26
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title></title>
+</head>
+<body>
+Hello word! This is api Server !
+http://ip:8080/api/members/123
+</body>
+</html>
---
+++ wxapp-manager-api/src/test/java/com/guonima/wxapp/util/OrderUtilTest.java
@@ -0,0 +1,32 @@
+package com.guonima.wxapp.util;
+
+import org.junit.Test;
+import org.junit.Before;
+import org.junit.After;
+
+/**
+ * OrderUtil Tester.
+ *
+ * @author <Authors name>
+ * @version 1.0
+ * @since <pre>���� 12, 2017</pre>
+ */
+public class OrderUtilTest {
+
+    @Before
+    public void before() throws Exception {
+    }
+
+    @After
+    public void after() throws Exception {
+    }
+
+    /**
+     * Method: createOrderNo()
+     */
+    @Test
+    public void testCreateOrderNo() throws Exception {
+        System.out.println(OrderUtil.createOrderNo());
+    }
+
+} 
