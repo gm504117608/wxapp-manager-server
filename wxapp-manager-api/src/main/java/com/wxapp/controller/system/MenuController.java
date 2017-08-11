10df57a4d895549c156fd33a7a93b58c7fa48fe8
gm504117608 <504117608@qq.com>
2017-08-11 18:16:23 +0800

    菜单功能编写

--- wxapp-manager-api/src/main/java/com/wxapp/controller/system/MenuController.java
+++ wxapp-manager-api/src/main/java/com/wxapp/controller/system/MenuController.java
@@ -8,6 +8,7 @@ import com.wxapp.util.MenuUtil;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
